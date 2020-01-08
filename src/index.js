d5e5f8da2b16ae2c6a69586c92755bc2612cc1a7
Chris Kang <christopha.kang@gmail.com>
2020-01-07 20:57:57 -0600

    creating new ApolloClient

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import 'dotenv/config';
+import 'dotenv/config'
+import 'cross-fetch/polyfill'
+import ApolloClient from 'apollo-boost'
 
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`
+      }
+    })
+  }
+})
+
+const userCredentials = { firstname: 'Robin' }
+const userDetails = { nationality: 'German' }
 
 const user = {
   ...userCredentials,
   ...userDetails,
-};
+}
 
-console.log(user);
+console.log(user)
 
-console.log(process.env.SOME_ENV_VARIABLE);
+console.log(process.env.SOME_ENV_VARIABLE)
