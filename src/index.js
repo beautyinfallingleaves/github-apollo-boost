1a373096319b68ee9c62c76b17f137f7a0db1777
Chris Kang <christopha.kang@gmail.com>
2020-01-10 19:01:40 -0600

    chained some queries, passing new pagination info to each

--- src/index.js
+++ src/index.js
@@ -20,7 +20,41 @@ client
   .query({
     query: GET_REPOSITORIES_OF_ORGANIZATION,
     variables: {
-      organization: 'the-road-to-learn-react'
+      organization: 'the-road-to-learn-react',
+      endCursor: undefined
     }
   })
-  .then(console.log);
+
+  .then(queryResult => {
+    const {pageInfo, edges} = queryResult.data.organization.repositories
+    const {endCursor, hasNextPage} = pageInfo
+    console.log('~~~ FIRST PAGE ~~~')
+    console.log('Edges:', edges.length)
+    console.log('End cursor:', endCursor)
+    console.log('Has Next Page:', hasNextPage)
+    return pageInfo
+  })
+
+  .then(({endCursor, hasNextPage}) => {
+    if(!hasNextPage) throw Error('no next page!')
+
+    return client.query({
+      query: GET_REPOSITORIES_OF_ORGANIZATION,
+      variables: {
+        organization: 'the-road-to-learn-react',
+        endCursor
+      }
+    })
+  })
+
+  .then(queryResult => {
+    const {pageInfo, edges} = queryResult.data.organization.repositories
+    const {endCursor, hasNextPage} = pageInfo
+    console.log('~~~ SECOND PAGE ~~~')
+    console.log('Edges:', edges.length)
+    console.log('End cursor:', endCursor)
+    console.log('Has Next Page:', hasNextPage)
+    return pageInfo
+  })
+
+  .catch(console.log)
--- src/script/queries.js
+++ src/script/queries.js
@@ -1,11 +1,11 @@
 import {gql} from 'apollo-boost'
 
 export const GET_REPOSITORIES_OF_ORGANIZATION = gql`
-  query FetchOrgRepos($organization: String!) {
+  query FetchOrgRepos($organization: String!, $endCursor: String) {
     organization(login: $organization) {
       name
       url
-      repositories(first: 5, orderBy: {field: STARGAZERS, direction: DESC}) {
+      repositories(first: 5, after: $endCursor, orderBy: {field: STARGAZERS, direction: DESC}) {
         edges {
           node {
             ...repository
