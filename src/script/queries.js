677e0e6437422486edc8ce8ac42f1608b4e10ba1
Chris Kang <christopha.kang@gmail.com>
2020-01-10 18:37:08 -0600

    added orderBy stargazer count

--- src/script/queries.js
+++ src/script/queries.js
@@ -2,17 +2,17 @@ import {gql} from 'apollo-boost'
 
 export const GET_REPOSITORIES_OF_ORGANIZATION = gql`
   query ($organization: String!) {
-  organization(login: $organization) {
-    name
-    url
-    repositories(first: 5) {
-      edges {
-        node {
-          name
-          url
+    organization(login: $organization) {
+      name
+      url
+      repositories(first: 5, orderBy: {field: STARGAZERS, direction: DESC}) {
+        edges {
+          node {
+            name
+            url
+          }
         }
       }
     }
   }
-}
 `
