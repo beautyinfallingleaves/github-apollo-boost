41f44aa7841e15c9cdb02d82e91c05a80b70fa79
Chris Kang <christopha.kang@gmail.com>
2020-01-10 18:46:13 -0600

    extracted repo fields as fragment; added pagination fields to query

--- src/script/queries.js
+++ src/script/queries.js
@@ -1,18 +1,26 @@
 import {gql} from 'apollo-boost'
 
 export const GET_REPOSITORIES_OF_ORGANIZATION = gql`
-  query ($organization: String!) {
+  query FetchOrgRepos($organization: String!) {
     organization(login: $organization) {
       name
       url
       repositories(first: 5, orderBy: {field: STARGAZERS, direction: DESC}) {
         edges {
           node {
-            name
-            url
+            ...repository
           }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
         }
       }
     }
+  }
+
+  fragment repository on Repository {
+    name
+    url
   }
 `
